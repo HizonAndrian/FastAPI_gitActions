fe54d55940394e6be2d072fe07148e94f768606e
[HizonAndrian] <[markandrianhizon@gmail.com]>
2025-10-29 15:06:26 +0800

    Added manual trigger.

--- main.py
+++ main.py
@@ -20,9 +20,13 @@ async def create_item(item: dict):
     result = await db.items.insert_one(item)
     return {"inserted_id": str(result.inserted_id)}
 
+
 @app.get("/get_items/")
 async def read_items():
+    # Create a list to hold the items
     items = []
+
+    # Cursor to iterate over the documents in the collection.
     cursor = db.items.find({})
     async for document in cursor:
         document["_id"] = str(document["_id"])  # Convert ObjectId to string
